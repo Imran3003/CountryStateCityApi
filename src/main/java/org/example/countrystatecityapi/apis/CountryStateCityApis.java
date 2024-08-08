c26f89a6f4258258dc7aa1cfa5ec120e1dce27ac
Mohamed Subaideen Imran <mohamedsubaideenimran@nmsworks.co.in>
2024-08-08 18:22:13 +0530

    Added @CrossOrigin annotation to resolve 'CORS policy' error.

--- src/main/java/org/example/countrystatecityapi/apis/CountryStateCityApis.java
+++ src/main/java/org/example/countrystatecityapi/apis/CountryStateCityApis.java
@@ -1,6 +1,7 @@
 package org.example.countrystatecityapi.apis;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import java.util.Map;
  * @created Jul 31, 2024
  */
 @RestController
+@CrossOrigin
 public class CountryStateCityApis
 {
     @Autowired
